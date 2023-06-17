fc4d6cc559a287323e2d51c5bea1c9a034bc29bc
Csderi <128956333+Csderi@users.noreply.github.com>
2023-06-17 15:54:09 -0600

    Add files via upload

---
+++ Agenda/Agenda.Android/SaveFileAndroid.cs
@@ -0,0 +1,25 @@
+﻿using Agenda.Droid.Services;
+using Agenda.Services;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SaveFileAndroid))]
+
+namespace Agenda.Droid.Services
+{
+    public class SaveFileAndroid : ISaveFile
+    {
+        public async Task SaveTextAsync(string filename, string text)
+        {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var filePath = Path.Combine(documentsPath, filename);
+
+            using (var writer = File.CreateText(filePath))
+            {
+                await writer.WriteAsync(text);
+            }
+        }
+    }
+}
